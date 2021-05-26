0de2a254fda0cb64e1f6343832dfe633dcf4071e
jion-lee <72534237+jion-lee@users.noreply.github.com>
2021-05-26 14:43:29 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace Web0526
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World! 222");
+                    await context.Response.WriteAsync("Hello World! 333");
                 });
             });
         }
